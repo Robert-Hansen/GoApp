0e5be32adc1fcbc75b2c4b44d0a3c3e1966ac7b0
DESKTOP-0D7LFVT\aintAlcoholic <rass255@hotmail.com>
2017-08-24 21:38:32 +0200

    no message

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ import (
 
 func main() {
 	fmt.Println("hejsdfsdsa123")
+
 	router := routes.NewRouter()
 	log.Println(router.HandleOPTIONS)
+
 	hej := middleware.Hej{"hej"}
 	log.Println(hej)
-	test := app.App{"App"}
-	log.Println(test)
+
+	app := app.App{"App"}
+	log.Println(app.Name)
 }
